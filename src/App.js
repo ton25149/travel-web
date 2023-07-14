4210b5df48a3d33b27609db5c4c6ca33e2290068
Patsawee <patsawee_web@hotmail.com>
2023-07-14 13:59:29 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './app.css'
+import Navbar from './Components/Navbar/Navbar'
+import Home from './Components/Home/Home'
+import Main from './Components/Main/Main'
+import Footer from './Components/Footer/Footer'
+
+const App = () => {
+  return (
+    <>
+    <Navbar/>
+    <Home />
+
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import './home.css'
+import video from '../../Assets/video2.mp4'
+import {ImLocation} from 'react-icons/im'
+
+const Home = () => {
+  return (
+   <section className='home'>
+    <div className='overlay'></div>
+    <video src={video} muted autoPlay loop type="video/mp4"></video>
+
+    <div className="homeContent container">
+      <div className="textDiv">
+        <span className="smallText">
+          Our Packages
+        </span>
+        <h1 className="homeTitle">
+          Search your Holiday
+        </h1>
+      </div>
+      <div className="cardDiv grid">
+        <div className="destinationInput">
+          <label htmlFor="city">Search your destination: asdasdasdasd</label>
+          <div className="input flex">
+            <input type="text" placeholder='Enter Name here'/>
+            <ImLocation className="icon"/>
+          </div>
+        </div>
+      </div>
+    </div>
+   </section>
+  )
+}
+
+export default Home
