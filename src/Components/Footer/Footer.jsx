fe29a9a48cbab066b5f8abd7c46489e2dce5d4ab
Patsawee <patsawee_web@hotmail.com>
2023-07-14 17:38:27 +0700

    edit

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import './footer.css'
+import video2 from '../../Assets/video2.mp4'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <section className='footer'>
+      <div className='videoDiv'>
+      <video src={video2} loop autoPlay muted type="video/mp4"></video>
+      </div>
+    </section>
   )
 }
 
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 import './home.css'
 import video from '../../Assets/video2.mp4'
 import {ImLocation} from 'react-icons/im'
+import {HiFilter} from 'react-icons/hi'
+import {TfiFacebook} from 'react-icons/tfi'
+import {FaTripadvisor} from 'react-icons/fa'
+import {TfiInstagram} from 'react-icons/tfi'
+import {TbApps} from 'react-icons/tb'
+import {AiOutlineUnorderedList} from 'react-icons/ai'
 
 const Home = () => {
   return (
@@ -25,6 +31,41 @@ const Home = () => {
             <input type="text" placeholder='Enter Name here'/>
             <ImLocation className="icon"/>
           </div>
+        </div>
+
+        <div className="dateInput">
+          <label htmlFor="date">Select Date:</label>
+          <div className="input flex">
+            <input type="date"/> 
+          </div>
+        </div>
+
+        <div className="priceInput">
+          <div className="label_total flex">
+            <label htmlFor="price">Max Price:
+            </label>
+            <h3 className='total'>$5000</h3>
+          </div>
+          <div className="input flex">
+            <input type="range" max="5000" min="500" />
+          </div>
+        </div>
+        <div className="searchOptions flex">
+          <HiFilter className="icon"/>
+          <span>MORE FILTERS</span>
+        </div>
+      </div>
+
+      <div className="homeFooterIcons flex">
+        <div className="rightIcons">
+        <TfiFacebook className="icon"/>
+        <TfiInstagram className="icon"/>
+        <FaTripadvisor className="icon"/>
+
+        </div>
+        <div className="leftIcons">
+        <AiOutlineUnorderedList className="icon"/>
+        <TbApps className="icon"/>
         </div>
       </div>
     </div>
