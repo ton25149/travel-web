46e62edde543ae1ae3663ef08fdfa0f49b259e87
Patsawee <patsawee_web@hotmail.com>
2023-07-16 01:23:50 +0700

    Final

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import './home.css'
 import video from '../../Assets/video2.mp4'
 import {ImLocation} from 'react-icons/im'
@@ -8,23 +8,30 @@ import {FaTripadvisor} from 'react-icons/fa'
 import {TfiInstagram} from 'react-icons/tfi'
 import {TbApps} from 'react-icons/tb'
 import {AiOutlineUnorderedList} from 'react-icons/ai'
+import Aos from 'aos'
+import 'aos/dist/aos.css'
 
 const Home = () => {
+
+useEffect(() =>{
+  Aos.init({duration: 2000})
+})
+
   return (
    <section className='home'>
     <div className='overlay'></div>
     <video src={video} muted autoPlay loop type="video/mp4"></video>
 
-    <div className="homeContent container">
+    <div  className="homeContent container">
       <div className="textDiv">
-        <span className="smallText">
+        <span data-aos="fade-up" className="smallText">
           Our Packages
         </span>
-        <h1 className="homeTitle">
+        <h1 data-aos="fade-up" className="homeTitle">
           Search your Holiday
         </h1>
       </div>
-      <div className="cardDiv grid">
+      <div data-aos="fade-up" className="cardDiv grid">
         <div className="destinationInput">
           <label htmlFor="city">Search your destination:</label>
           <div className="input flex">
@@ -56,7 +63,7 @@ const Home = () => {
         </div>
       </div>
 
-      <div className="homeFooterIcons flex">
+      <div data-aos="fade-up" className="homeFooterIcons flex">
         <div className="rightIcons">
         <TfiFacebook className="icon"/>
         <TfiInstagram className="icon"/>
